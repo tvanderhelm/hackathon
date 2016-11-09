a875c8fc820cb5b53ffaa00f911b3dfc544f19d0
Radjino Bholanath <radjino@gamebasics.nl>
2016-11-09 16:16:06 +0100

    Ohjazekers

--- Stadium3D/Assets/Scripts/CameraMovement.cs
+++ Stadium3D/Assets/Scripts/CameraMovement.cs
@@ -4,25 +4,37 @@ public class CameraMovement : MonoBehaviour
 {
     public float perspectiveZoomSpeed = 0.5f;
     public float orthoZoomSpeed = 0.5f;
-    public float rotateSpeed = 500f;
+    public float rotateSpeed = 10f;
+    public float minZoomLevel = 15f;
+    public float maxZoomLevel = 80f;
+    public float touchRotateMultiplier = 2f;
+
+    private Vector3 point;
 
     public GameObject stadium;
 
+    private void Start()
+    {
+        point = stadium.transform.position;
+        transform.LookAt(point);
+    }
+
     private void Update()
     {
-        transform.LookAt(stadium.transform);
-        transform.Translate(Vector3.right * Time.deltaTime*rotateSpeed);
-
-        if (Input.touchCount == 1)
+        if (Input.touchCount < 2)
         {
-            //var touchZero = Input.GetTouch(0);
+            float rotateMultiplier = 1f;
 
-            //var movementX = touchZero.position.x - touchZero.deltaPosition.x;
-            //var movementY = touchZero.position.y - touchZero.deltaPosition.y;
+            if (Input.touchCount == 1)
+            {
+                var touchZero = Input.GetTouch(0);
 
-            //var cameraComponent = GetComponent<Camera>();
+                Debug.Log(touchZero.deltaPosition);
 
-            //cameraComponent.transform.Translate(-movementX/100, 0, -movementY/100);
+                rotateMultiplier = touchZero.deltaPosition.x * touchRotateMultiplier;
+            }
+
+            transform.RotateAround(point, new Vector3(0.0f, 1.0f, 0.0f), 20 * Time.deltaTime * rotateSpeed * rotateMultiplier);
         }
         else if (Input.touchCount == 2)
         {
@@ -42,12 +54,12 @@ public class CameraMovement : MonoBehaviour
             if (cameraComponent.orthographic)
             {
                 cameraComponent.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
-                cameraComponent.orthographicSize = Mathf.Max(cameraComponent.orthographicSize, 15f);
+                cameraComponent.orthographicSize = Mathf.Max(cameraComponent.orthographicSize, minZoomLevel);
             }
             else
             {
                 cameraComponent.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
-                cameraComponent.fieldOfView = Mathf.Clamp(cameraComponent.fieldOfView, 15f, 79.9f);
+                cameraComponent.fieldOfView = Mathf.Clamp(cameraComponent.fieldOfView, minZoomLevel, maxZoomLevel);
             }
         }
     }
