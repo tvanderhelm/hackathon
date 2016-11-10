4fbb00e0c06640f89ec4a1b4df20fa01194552b3
Sjoerd <sjoerdmoret6@gmail.com>
2016-11-10 13:42:21 +0100

    fuck you tim, wij kunnen het wel

--- Stadium3D/Assets/Scripts/CameraMovement.cs
+++ Stadium3D/Assets/Scripts/CameraMovement.cs
@@ -10,74 +10,106 @@ public class CameraMovement : MonoBehaviour
     public float minZoomLevel = 15f;
     public float maxZoomLevel = 80f;
     public float touchRotateMultiplier = 2f;
+    public float zoomToFieldMultiplier = 8f;
+    public float zoomToFieldOfView = -0.5f;
+    public float rotatingToStartMultiplier = 20f;
+
+    public bool zoomtoField = false;
 
     private Vector3 point;
 
     public GameObject stadium;
+    public Camera cameraComponent;
 
     private void Start()
     {
+        cameraComponent = GetComponent<Camera>();
         point = stadium.transform.position;
         transform.LookAt(point);
     }
 
     private void Update()
     {
-        if (Input.touchCount < 2 && rotating)
+        if (zoomtoField)
         {
-            float rotateMultiplier = 1f;
+            transform.RotateAround(point, new Vector3(-1.0f, 0.0f, 0.0f), 20 * Time.deltaTime * rotateSpeed * zoomToFieldMultiplier);
+            if (transform.position.y > 9000 && transform.position.y < 10300)
+            {
+                Debug.Log(transform.position.y);
+                if (cameraComponent.orthographic)
+                {
+                    cameraComponent.orthographicSize += zoomToFieldOfView * orthoZoomSpeed;
+                    cameraComponent.orthographicSize = Mathf.Max(cameraComponent.orthographicSize, minZoomLevel);
+                }
+                else
+                {
+                    cameraComponent.fieldOfView += zoomToFieldOfView * perspectiveZoomSpeed;
+                    cameraComponent.fieldOfView = Mathf.Clamp(cameraComponent.fieldOfView, minZoomLevel, maxZoomLevel);
+                }
+            }
+            if (transform.position.y > 10300)
+            {
+                zoomtoField = false;
+            }
+        }
+        else
+        {
+            var rotateMultiplier = 1f;
+            if (Input.touchCount < 2 && rotating)
+            {
+                var multip = 1;
 
-            if (Input.touchCount == 1 && !rotatingToStart)
+                if (Input.touchCount == 1 && !rotatingToStart)
+                {
+                    var touchZero = Input.GetTouch(0);
+
+                    rotateMultiplier = touchZero.deltaPosition.x * touchRotateMultiplier;
+                }
+                if (rotatingToStart)
+                {
+                    rotateMultiplier *= rotatingToStartMultiplier;
+                    if (transform.rotation.x < 0)
+                    {
+                        multip *= -1;
+                    }
+                }
+                if (rotatingToStart && (transform.rotation.y > 0.98 || transform.rotation.y < -0.98))
+                {
+                    rotating = false;
+                    zoomtoField = true;
+                }
+                else
+                {
+                    Debug.Log(rotateMultiplier);
+
+                    transform.RotateAround(point, new Vector3(0.0f, multip * 1.0f, 0.0f), 20f * Time.deltaTime * rotateSpeed * rotateMultiplier);
+                }
+            }
+            else if (Input.touchCount == 2)
             {
                 var touchZero = Input.GetTouch(0);
+                var touchOne = Input.GetTouch(1);
 
-                Debug.Log(touchZero.deltaPosition);
+                var touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+                var touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
 
-                rotateMultiplier = touchZero.deltaPosition.x * touchRotateMultiplier;
-            }
-            else if (rotatingToStart)
-                rotateMultiplier *= 20f;
+                var prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+                var touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
 
-            transform.RotateAround(point, new Vector3(0.0f, 1.0f, 0.0f), 20 * Time.deltaTime * rotateSpeed * rotateMultiplier);
+                var deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
 
-            if (rotatingToStart && (transform.position.x > 100 && transform.position.x < 160) && (transform.position.z > 9500 && transform.position.z < 9600))
-            {
-                rotating = false;
-            }
-        }
-        else if (Input.touchCount == 2)
-        {
-            var touchZero = Input.GetTouch(0);
-            var touchOne = Input.GetTouch(1);
-
-            var touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-            var touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
-
-            var prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-            var touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
-
-            var deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
-
-            var cameraComponent = GetComponent<Camera>();
-
-            if (cameraComponent.orthographic)
-            {
-                cameraComponent.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
-                cameraComponent.orthographicSize = Mathf.Max(cameraComponent.orthographicSize, minZoomLevel);
-            }
-            else
-            {
-                cameraComponent.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
-                cameraComponent.fieldOfView = Mathf.Clamp(cameraComponent.fieldOfView, minZoomLevel, maxZoomLevel);
+                if (cameraComponent.orthographic)
+                {
+                    cameraComponent.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+                    cameraComponent.orthographicSize = Mathf.Max(cameraComponent.orthographicSize, minZoomLevel);
+                }
+                else
+                {
+                    Debug.Log(deltaMagnitudeDiff);
+                    cameraComponent.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
+                    cameraComponent.fieldOfView = Mathf.Clamp(cameraComponent.fieldOfView, minZoomLevel, maxZoomLevel);
+                }
             }
         }
-    }
-
-    // convert screen point to world point
-    private Vector2 getWorldPoint(Vector2 screenPoint)
-    {
-        RaycastHit hit;
-        Physics.Raycast(Camera.main.ScreenPointToRay(screenPoint), out hit);
-        return hit.point;
     }
 }
