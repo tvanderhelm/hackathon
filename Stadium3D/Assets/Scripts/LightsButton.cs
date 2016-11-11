8cbb15a08de9ae866e4fc5db47eee4c21d97c61f
Radjino Bholanath <radjino@gamebasics.nl>
2016-11-11 14:45:33 +0100

    Night mode + enable/disable

---
+++ Stadium3D/Assets/Scripts/LightsButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class LightsButton : MonoBehaviour
+{
+    public Light Sun;
+    public Light UpperRightLight;
+    public Light LowerRightLight;
+    public Light UpperLeftLight;
+    public Light LowerLeftLight;
+
+    private GameObject g;
+
+    public void OnClick()
+    {
+        Sun.enabled = !Sun.enabled;
+        UpperRightLight.enabled = !UpperRightLight.enabled;
+        LowerRightLight.enabled = !LowerRightLight.enabled;
+        UpperLeftLight.enabled = !UpperLeftLight.enabled;
+        LowerLeftLight.enabled = !LowerLeftLight.enabled;
+
+        DisableHalo(UpperRightLight);
+        DisableHalo(LowerRightLight);
+        DisableHalo(UpperLeftLight);
+        DisableHalo(LowerLeftLight);
+    }
+
+    private void DisableHalo(Light lightSource)
+    {
+        var halo = (Behaviour) lightSource.GetComponent("Halo");
+        halo.enabled = !halo.enabled;
+    }
+}
