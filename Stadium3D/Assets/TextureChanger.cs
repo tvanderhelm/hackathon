8d9dffcd3e39a8cefa5072daceb862e004b000c5
ne-alex-gamebasics <alexn@gamebasics.nl>
2016-11-11 11:45:21 +0100

    texture change

---
+++ Stadium3D/Assets/TextureChanger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureChanger : MonoBehaviour {
+    public Texture[] textures;
+    public int currentTexture;
+    public Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<Renderer>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space)) {
+            currentTexture++;
+            currentTexture %= textures.Length;
+            renderer.material.mainTexture = textures[currentTexture];   
+        }
+	}
+}
