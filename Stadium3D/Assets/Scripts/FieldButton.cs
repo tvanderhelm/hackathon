4499f8b1bccfd25179a8ba4ae4b5bb792cedf77f
Radjino Bholanath <radjino@gamebasics.nl>
2016-11-11 11:09:21 +0100

    Camera starts further zoomed out, fix animations

--- Stadium3D/Assets/Scripts/FieldButton.cs
+++ Stadium3D/Assets/Scripts/FieldButton.cs
@@ -1,10 +1,20 @@
 ﻿using UnityEngine;
+
 public class FieldButton : MonoBehaviour
 {
     public void OnClick()
     {
-        var cameraScript = Camera.main.gameObject.GetComponent<CameraMovement>();
+        var mainCamera = Camera.main;
+        var cameraScript = mainCamera.gameObject.GetComponent<CameraMovement>();
+        
+        if (cameraScript.rotating && cameraScript.rotatingToStart == false)
+        {
+            cameraScript.rotatingToStart = true;
 
-       cameraScript.rotatingToStart = true;
+            if (mainCamera.transform.position.x > 0)
+            {
+                cameraScript.shouldInvert = true;
+            }
+        }
     }
 }
