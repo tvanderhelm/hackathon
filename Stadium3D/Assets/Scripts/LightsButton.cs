b562a8a7b05104f7a3859621ff5771446f9cd960
Radjino Bholanath <radjino@gamebasics.nl>
2016-11-11 15:14:47 +0100

    Day night cycle

--- Stadium3D/Assets/Scripts/LightsButton.cs
+++ Stadium3D/Assets/Scripts/LightsButton.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class LightsButton : MonoBehaviour
 {
@@ -8,8 +9,26 @@ public class LightsButton : MonoBehaviour
     public Light UpperLeftLight;
     public Light LowerLeftLight;
 
-    private GameObject g;
+    private void Start()
+    {
+        if (DateTime.Now.Hour > 14 || DateTime.Now.Hour < 7)
+        {
+            Sun.enabled = false;
+            UpperRightLight.enabled = true;
+            LowerRightLight.enabled = true;
+            UpperLeftLight.enabled = true;
+            LowerLeftLight.enabled = true;
 
+            ToggleHalo(UpperRightLight, true);
+            ToggleHalo(LowerRightLight, true);
+            ToggleHalo(UpperLeftLight, true);
+            ToggleHalo(LowerLeftLight, true);
+        }
+    }
+
+    /// <summary>
+    /// Toggle lights.
+    /// </summary>
     public void OnClick()
     {
         Sun.enabled = !Sun.enabled;
@@ -18,15 +37,26 @@ public class LightsButton : MonoBehaviour
         UpperLeftLight.enabled = !UpperLeftLight.enabled;
         LowerLeftLight.enabled = !LowerLeftLight.enabled;
 
-        DisableHalo(UpperRightLight);
-        DisableHalo(LowerRightLight);
-        DisableHalo(UpperLeftLight);
-        DisableHalo(LowerLeftLight);
+        ToggleHalo(UpperRightLight);
+        ToggleHalo(LowerRightLight);
+        ToggleHalo(UpperLeftLight);
+        ToggleHalo(LowerLeftLight);
     }
 
-    private void DisableHalo(Light lightSource)
+    /// <summary>
+    /// Toggle halo of one specific light. Can force the halo to be enabled.
+    /// </summary>
+    private void ToggleHalo(Light lightSource, bool forceEnable = false)
     {
         var halo = (Behaviour) lightSource.GetComponent("Halo");
-        halo.enabled = !halo.enabled;
+
+        if (forceEnable)
+        {
+            halo.enabled = true;
+        }
+        else
+        {
+            halo.enabled = !halo.enabled;
+        }
     }
 }
