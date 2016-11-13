48dfe8a7d40f5f08fee2bedda94ca4e4ed9b5d8f
Willem <willem@willem>
2016-11-13 17:37:47 +0100

    Stadium upgrade etc

---
+++ Stadium3D/Assets/Scripts/UpgradeStadium.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class UpgradeStadium : MonoBehaviour
+{
+
+	public GameObject stadiumSmall;
+	public GameObject stadiumHuge;
+
+	public void OnClick()
+	{
+		var anim = stadiumSmall.GetComponent<Animation>();
+		anim.Play("stadium_disappear");
+	}
+
+	public void AnimComplete(int phase)
+	{
+		stadiumSmall.SetActive(false);
+		stadiumHuge.SetActive(true);
+		var anim = stadiumHuge.GetComponent<Animation>();
+		anim.Play("stadium_appear");
+	}
+}
