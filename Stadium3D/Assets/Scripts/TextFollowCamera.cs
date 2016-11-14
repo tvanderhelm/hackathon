e9bff83453d43fca5d5280b8d74a8b471ae3ae4e
Radjino Bholanath <radjino@gamebasics.nl>
2016-11-14 11:52:18 +0100

    Text follow camera in 3D world

---
+++ Stadium3D/Assets/Scripts/TextFollowCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class TextFollowCamera : MonoBehaviour
+{
+    void Update()
+    {
+        var lookPosition = Camera.main.transform.position - transform.position;
+        lookPosition.y = 0;
+        var rotation = Quaternion.LookRotation(lookPosition);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime);
+    }
+}
