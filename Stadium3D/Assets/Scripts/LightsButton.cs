771fad442b16c920900d0d53085e812d48cebec8
GBWS43\Tim <tim@gamebasics.nl>
2016-11-14 14:45:57 +0100

    added light fpor big stadium

--- Stadium3D/Assets/Scripts/LightsButton.cs
+++ Stadium3D/Assets/Scripts/LightsButton.cs
@@ -9,6 +9,11 @@ public class LightsButton : MonoBehaviour
     public Light UpperLeftLight;
     public Light LowerLeftLight;
 
+    public Light[] bigLights;
+    public Behaviour[] bigHalo;
+
+    private int currentLight;
+    private bool smallActive = false;
     private bool showHalo = true;
 
     private void Start()
@@ -34,19 +39,70 @@ public class LightsButton : MonoBehaviour
     public void OnClick()
     {
         Sun.enabled = !Sun.enabled;
-        UpperRightLight.enabled = !UpperRightLight.enabled;
-        LowerRightLight.enabled = !LowerRightLight.enabled;
-        UpperLeftLight.enabled = !UpperLeftLight.enabled;
-        LowerLeftLight.enabled = !LowerLeftLight.enabled;
+        
 
-        if (showHalo)
+        if (!smallActive)
         {
-            ToggleHalo(UpperRightLight);
-            ToggleHalo(LowerRightLight);
-            ToggleHalo(UpperLeftLight);
-            ToggleHalo(LowerLeftLight);
+            toggleBigLights();
+
+        } else
+        {
+            UpperRightLight.enabled = !UpperRightLight.enabled;
+            LowerRightLight.enabled = !LowerRightLight.enabled;
+            UpperLeftLight.enabled = !UpperLeftLight.enabled;
+            LowerLeftLight.enabled = !LowerLeftLight.enabled;
+
+
+            if (showHalo)
+            {
+                ToggleHalo(UpperRightLight);
+                ToggleHalo(LowerRightLight);
+                ToggleHalo(UpperLeftLight);
+                ToggleHalo(LowerLeftLight);
+            }
         }
+        
+
+       
     }
+
+
+    private void toggleBigLights(bool force = false, bool forceState = true)
+    {
+
+        while (currentLight < bigLights.Length)
+        {
+            if (force)
+            {
+                bigLights[currentLight].enabled = forceState;
+            } else
+            {
+                bigLights[currentLight].enabled = !(bigLights[currentLight].enabled);
+            }
+            ToggleHalo(bigLights[currentLight], force, forceState);
+
+            currentLight++;
+
+        }
+        currentLight = 0;
+        while (currentLight < bigHalo.Length)
+        {
+            ToggleBigHalo(bigHalo[currentLight], force, forceState);
+            currentLight++;
+
+        }
+        currentLight = 0;
+
+    }
+
+    private void toggleSmallLights(bool force = false, bool forceState = true)
+    {
+        ToggleHalo(UpperRightLight, force, forceState);
+        ToggleHalo(LowerRightLight, force, forceState);
+        ToggleHalo(UpperLeftLight, force, forceState);
+        ToggleHalo(LowerLeftLight, force, forceState);
+    }
+
 
     /// <summary>
     /// Toggle halo of one specific light. Can force the halo to be enabled or disabled.
@@ -66,34 +122,43 @@ public class LightsButton : MonoBehaviour
     }
 
     /// <summary>
+    /// Toggle halo of one specific light. Can force the halo to be enabled or disabled.
+    /// </summary>
+    private void ToggleBigHalo(Behaviour halo, bool force = false, bool forceState = true)
+    {
+
+        if (force)
+        {
+            halo.enabled = forceState;
+        }
+        else
+        {
+            halo.enabled = !halo.enabled;
+        }
+    }
+
+    /// <summary>
     /// Turn the halo's on or off if the stadium is upgraded.
     /// </summary>
     /// <param name="small"></param>
     public void SwitchStadium(bool small)
     {
-        if (small)
-        {
-            // Enable the halo's.
-            showHalo = true;
 
-            if (!Sun.enabled)
+        smallActive = small;
+        toggleSmallLights(true, false);
+        toggleBigLights(true, false);
+
+        if (!Sun.isActiveAndEnabled)
+        {
+            if (small)
             {
-                ToggleHalo(UpperRightLight, true);
-                ToggleHalo(LowerRightLight, true);
-                ToggleHalo(UpperLeftLight, true);
-                ToggleHalo(LowerLeftLight, true);
+                toggleSmallLights(true, true);
+            } else
+            {
+                toggleBigLights(true, true);
             }
-            
         }
-        else
-        {
-            // Turn all halo's off.
-            ToggleHalo(UpperRightLight, true, false);
-            ToggleHalo(LowerRightLight, true, false);
-            ToggleHalo(UpperLeftLight, true, false);
-            ToggleHalo(LowerLeftLight, true, false);
 
-            showHalo = false;
-        }
+        
     }
 }
